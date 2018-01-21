bcb673746c8d50354f90d92926079ec98d6ed3e9
Adrian Biedrzycki <a@modules.io>
2018-01-21 02:44:42 -0800

    Make example use data url

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
+const fs = require('fs');
 const Worker = require('.');
 
-const worker = new Worker('example-worker.js', {
+const worker = new Worker('data:application/javascript;base64,' + fs.readFileSync('example-worker.js', 'base64'), {
   baseUrl: 'https://unpkg.com/window-worker'
 });
 worker.onmessage = msg => {
