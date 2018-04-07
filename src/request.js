0207838d4270c26f1d3d987cb5ae107b8511ea37
Avaer Kazmer <a@modules.io>
2018-04-07 07:04:47 -0400

    Bugfix request script osx hang

--- src/request.js
+++ src/request.js
@@ -7,20 +7,24 @@ if (process.argv.length === 3) {
       const res = await fetch(url);
       if (res.status >= 200 && res.status < 300) {
         const text = await res.text();
-        process.stdout.write(text);
-        process.exit(0);
+        process.stdout.write(text, () => {
+          process.exit(0);
+        });
       } else {
         const err = new Error('request got invalid status code: ' + res.status);
-        process.stderr.write(err.stack);
-        process.exit(1);
+        process.stderr.write(err.stack, () => {
+          process.exit(1);
+        });
       }
     } catch(err) {
-      process.stderr.write(err.stack);
-      process.exit(1);
+      process.stderr.write(err.stack, () => {
+        process.exit(1);
+      });
     }
   })();
 } else {
   const err = new Error('invalid arguments: ' + process.argv.length);
-  process.stderr.write(err.stack);
-  process.exit(1);
+  process.stderr.write(err.stack, () => {
+    process.exit(1);
+  });
 }
